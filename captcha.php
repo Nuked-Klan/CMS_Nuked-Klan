0c1fa723c151bb400bff8574196597d3caf5537e
sekuline <sekuline@gmail.com>
2011-05-04 23:46:03 +0200

    nouveau captcha en noir / blanc. plus lisible.

--- captcha.php
+++ captcha.php
@@ -14,16 +14,16 @@ $text = $_SESSION['captcha'];
 header("Content-type: image/png");
 $im = imagecreatefromjpeg("images/captcha.jpg");
 $id = imagecreatefromjpeg("images/captcha.jpg");
-$grey = imagecolorallocate($im, 128, 128, 128);
-$black = imagecolorallocate($im, 0, 0, 0);
-$font = 'Includes/font/Alanden_.ttf';
+$grey = imagecolorallocate($im, 0, 0, 0);
+$black = imagecolorallocate($im, 200, 200, 200);
+$font = 'Includes/font/eurof75.ttf';
 
 for($i=0;$i<5;$i++)
 {
-	$angle = mt_rand(10,30);
+	$angle = mt_rand(5,30);
 if (mt_rand(0,1) == 1) $angle =- $angle;
-	imagettftext($im, 12, $angle, 11+(20*$i), 15, $grey, $font, substr($text,$i,1));
-	imagettftext($im, 12, $angle, 10+(20*$i), 16, $black, $font, substr($text,$i,1));
+	imagettftext($im, 12, $angle, 11+(22*$i), 15, $grey, $font, substr($text,$i,1));
+	imagettftext($im, 12, $angle, 10+(22*$i), 16, $black, $font, substr($text,$i,1));
 }
 
 imagecopymerge ( $im, $id, 0, 0, 0, 0, 120, 30, 50 );
