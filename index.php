6a91bea2ec00232357028be7436b0fd4fc5c27bb
sekuline <sekuline@gmail.com>
2011-05-09 01:35:43 +0200

    Optimisation globale d'NK.

--- index.php
+++ index.php
@@ -8,49 +8,45 @@
 // the Free Software Foundation; either version 2 of the License.           //
 // -------------------------------------------------------------------------//
 
-define ("INDEX_CHECK", 1);
-$mtime = microtime();
-include_once ('Includes/php51compatibility.php');
-include ("globals.php");
-@include ("conf.inc.php");
-//For compatibility with all old module and theme
-if (defined('COMPATIBILITY_MODE') && COMPATIBILITY_MODE == true)
-	extract($_REQUEST);
+define("INDEX_CHECK", 1);
 
-if (!defined("NK_INSTALLED"))
+$mtime = microtime();
+
+include_once ('Includes/php51compatibility.php');
+include ('globals.php');
+@include ('conf.inc.php');
+// POUR LA COMPATIBILITE DES ANCIENS THEMES ET MODULES - FOR COMPATIBITY WITH ALL OLD MODULE AND THEME
+if (defined('COMPATIBILITY_MODE') && COMPATIBILITY_MODE == true) extract($_REQUEST);
+
+if (!defined('NK_INSTALLED'))
 {
-	if (file_exists("install.php"))
+	if (file_exists('install.php'))
 	{
-		header("location: install.php");
-		exit(); //Run
+		header('location: install.php');
+		exit();
 	}
 }
 else
 {
-	if (file_exists("update.php")
-		&&
-		(
-			(file_exists("install.php") && !unlink("install.php"))
-			|| (file_exists("update.php") && !unlink("update.php"))
-		))
+	if (file_exists('update.php') && file_exists('install.php') || file_exists('update.php') || file_exists('install.php'))
 	{
-		echo "<br /><br /><br /><div style=\"text-align: center;\"><big>Warning ! <b>install.php</b> and <b>update.php</b> must be removed before continuing !</big></div>";
+		echo '<div style="text-align: center; margin-top: 15px"><big>Warning ! <b>install.php</b> and <b>update.php</b> must be removed before continuing !</big></div>';
 		exit();
 	}
 }
-if (!defined("NK_OPEN"))
+
+if (!defined('NK_OPEN'))
 {
-	echo "<br /><br /><br /><div style=\"text-align: center;\"><big>Sorry, this website is momently closed, Please try again later.</big></div>";
+	echo '<div style="text-align: center; margin-top: 15px"><big>Sorry, this website is momently closed, Please try again later.</big></div>';
 	exit();
 }
 
-date_default_timezone_set('Europe/Paris');
-include ("nuked.php");
-//error_reporting(E_ALL);
+include ('nuked.php');
 include_once ('Includes/hash.php');
 
-//Gestion des erreus sql
-set_error_handler("erreursql");
+// GESTION DES ERREURS SQL - SQL ERROR MANAGEMENT
+set_error_handler('erreursql');
+
 $session = session_check();
 if ($session == 1) $user = secure();
 else $user = array();
@@ -58,21 +54,20 @@ else $user = array();
 $session_admin = admin_check();
 $check_ip = banip();
 
-if ($check_ip != "")
+if (!empty($check_ip))
 {
-	$url_ban = "ban.php?ip_ban=" . $check_ip;
+	$url_ban = 'ban.php?ip_ban=' . $check_ip;
 	redirect($url_ban, 0);
 	exit();
 }
 
 if (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] != "") $_REQUEST['im_file'] = $_REQUEST['nuked_nude'];
 else if (isset($_REQUEST['page']) && $_REQUEST['page'] != "") $_REQUEST['im_file'] = $_REQUEST['page'];
-else $_REQUEST['im_file'] = "index";
+else $_REQUEST['im_file'] = 'index';
 
-// Securite par phpSecure.info
-if (preg_match("`\.\.`", $theme) || preg_match("`\.\.`", $language) || preg_match("`\.\.`", $_REQUEST['file']) || preg_match("`\.\.`", $_REQUEST['im_file']) || preg_match("`http\:\/\/`i", $_REQUEST['file']) || preg_match("`http\:\/\/`i", $_REQUEST['im_file'] || strpos( $_SERVER['QUERY_STRING'], ".." ) || strpos( $_SERVER['QUERY_STRING'], "http://" ) || strpos( $_SERVER['QUERY_STRING'], "%3C%3F" )))
+if (preg_match('`\.\.`', $theme) || preg_match('`\.\.`', $language) || preg_match('`\.\.`', $_REQUEST['file']) || preg_match('`\.\.`', $_REQUEST['im_file']) || preg_match('`http\:\/\/`i', $_REQUEST['file']) || preg_match('`http\:\/\/`i', $_REQUEST['im_file'] || strpos( $_SERVER['QUERY_STRING'], '..' ) || strpos( $_SERVER['QUERY_STRING'], 'http://' ) || strpos( $_SERVER['QUERY_STRING'], '%3C%3F' )))
 {
-	die("<br /><br /><br /><div style=\"text-align: center;\"><big>What are you trying to do ?</big></div>");
+	die('<div style="text-align: center; margin-top: 15px"><big>What are you trying to do ?</big></div>');
 }
 
 $_REQUEST['file'] = basename(trim($_REQUEST['file']));
@@ -86,224 +81,157 @@ if (!$user)
 	$visiteur = 0;
 	$_SESSION['admin'] = false;
 }
-else
-{
-	$visiteur = $user[1];
-}
-if ($nuked['nk_status'] == "closed" && $user[1] < 9 && $_REQUEST['op'] != "login_screen" && $_REQUEST['op'] != "login_message" && $_REQUEST['op'] != "login")
-{
-	include ("themes/" . $theme . "/colors.php");
-	translate("lang/" . $language . ".lang.php");
+else $visiteur = $user[1];
 
-	echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
-	. "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\">\n"
-	. "<head><title>" . $nuked['name'] . " :: " . $nuked['slogan'] . " ::</title>\n"
-	. "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />\n"
-	. "<meta http-equiv=\"content-style-type\" content=\"text/css\" />\n"
-	. "<link title=\"style\" type=\"text/css\" rel=\"stylesheet\" href=\"themes/" . $theme . "/style.css\" />\n"
-	. "<body style=\"background: " . $bgcolor2 . ";\"><div><br /><br /><br /><br /><br /><br /><br /><br /></div>\n"
-	. "<table style=\"margin-left: auto;margin-right: auto;text-align: left;background: " . $bgcolor3 . ";\" cellspacing=\"1\" cellpadding=\"20\">\n"
-	. "<tr><td style=\"background: " . $bgcolor1 . ";\" align=\"center\"><big><b>" . $nuked['name'] . " - " . $nuked['slogan'] . "</b></big><br /><br />\n"
-	. _SITECLOSED . "</td></tr></table></body></html>";
-}
-else if (($_REQUEST['file'] == "Admin" || $_REQUEST['page'] == "admin" || (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == "admin")) && $session_admin == '')
+include ('themes/' . $theme . '/colors.php');
+translate('lang/' . $language . '.lang.php');
+
+if ($nuked['nk_status'] == 'closed' && $user[1] < 9 && $_REQUEST['op'] != 'login_screen' && $_REQUEST['op'] != 'login_message' && $_REQUEST['op'] != 'login')
 {
-	include ("themes/" . $theme . "/colors.php");
-	include ("themes/" . $theme . "/theme.php");
-	translate("lang/" . $language . ".lang.php");
+
+	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	<head><title>' . $nuked['name'] . ' - ' . $nuked['slogan'] . '</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link title="style" type="text/css" rel="stylesheet" href="themes/' . $theme . '/style.css" />
+	<body style="background: ' . $bgcolor2 . '">
+	<div style="width: 600px; padding: 25px; margin: 40px auto; border: 1px solid ' . $bgcolor3 . '; background: ' . $bgcolor1 . '; text-align: center">
+	<h2 style="margin: 0">' . $nuked['name'] . ' - ' . $nuked['slogan'] . '</h2>
+	' . _SITECLOSED . '</div></body></html>';
+}
+else if (($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin' || (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == 'admin')) && $session_admin == '')
+{
+	include ('themes/' . $theme . '/theme.php');
 
 	if (!isset($_REQUEST['nuked_nude'])) top();
-	include("modules/Admin/login.php");
+	include('modules/Admin/login.php');
 
 	if (!isset($_REQUEST['nuked_nude']))
 	{
 		footer();
-		include("Includes/copyleft.php");
+		include('Includes/copyleft.php');
 	}
 }
 else if (($_REQUEST['file'] != "Admin" AND $_REQUEST['page'] != "admin") || ( nivo_mod($_REQUEST['file']) === false || (nivo_mod($_REQUEST['file']) > -1 && (nivo_mod($_REQUEST['file']) <= $visiteur))) )
 {
-
-	include ("themes/" . $theme . "/colors.php");
 	include ("themes/" . $theme . "/theme.php");
-	translate("lang/" . $language . ".lang.php");
 
 	if ($nuked['level_analys'] != -1) visits();
 
 	if (!isset($_REQUEST['nuked_nude']))
 	{
-
-		if (defined("NK_GZIP") && @extension_loaded('zlib') && !@ini_get('zlib.output_compression') && @phpversion() >= "4.0.4" && stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false)
+		if (defined("NK_GZIP") && @extension_loaded('zlib') && @phpversion() >= "4.0.4" && stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false)
 		{
 			ob_start("ob_gzhandler");
 		}
-		else
-		{
-			ob_start();
-		}
+		elseif (!@ini_get('zlib.output_compression')) ob_start();
 
-		if (!($_REQUEST['file'] == "Admin" || $_REQUEST['page'] == "admin" || (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == "admin")) || $_REQUEST['page'] == "login")
+		if (!($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin' || (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == 'admin')) || $_REQUEST['page'] == 'login')
 		{
 			top();
 		}
-		echo "<script type=\"text/javascript\" src=\"js/infobulle.js\"></script>"
-		. "<script type=\"text/javascript\">InitBulle('" . $bgcolor2 . "', '" . $bgcolor3 . "', 2);</script>\n"
-		."<link rel=\"stylesheet\" href=\"editeur/plugins/insertcode/insertcode.css\" type=\"text/css\" media=\"screen\" />\n";
+
+		echo '<script type="text/javascript" src="js/infobulle.js"></script>
+		<script type="text/javascript">InitBulle(\'' . $bgcolor2 . '\', \'' . $bgcolor3 . '\', 2);</script>
+		<link rel="stylesheet" href="editeur/plugins/insertcode/insertcode.css" type="text/css" media="screen" />';
+
 		if (!($_REQUEST['file'] == "Admin" || $_REQUEST['page'] == "admin" || (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == "admin")) || $_REQUEST['page'] == "login")
 		{
 		?>
-		<script type="text/javascript" src="editeur/tiny_mce.js"></script>
-		<script type="text/javascript">
-			tinyMCE.init({
-		// General options
-		mode : "textareas",
-		theme : "advanced",
-		<?php
-		if($language == "french")
-		{
-		?>
-		language : "fr",
-		<?php
-		}
-		?>
-		plugins : "pagebreak,layer,table,save,advimage,advlink,emotions,spellchecker,inlinepopups,preview,print,contextmenu,paste,directionality,fullscreen,wordcount,advlist,autosave",
-		editor_selector : "editoradvanced",
-		// Theme options
-		<?php
-		$sql1 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'couleur'");
-		list($couleur) = mysql_fetch_array($sql1);
-		$sql2 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'bouton'");
-		list($bouton) = mysql_fetch_array($sql2);
-		$sql3 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'status'");
-		list($status) = mysql_fetch_array($sql3);
-		$sql5 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'ligne1'");
-		list($ligne1) = mysql_fetch_array($sql5);
-		$sql6 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'ligne2'");
-		list($ligne2) = mysql_fetch_array($sql6);
-		$sql7 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'ligne3'");
-		list($ligne3) = mysql_fetch_array($sql7);
-		$sql8 = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name = 'ligne4'");
-		list($ligne4) = mysql_fetch_array($sql8);
-		?>
-		theme_advanced_buttons1 : "<?php echo $ligne1; ?>",
-		theme_advanced_buttons2 : "<?php echo $ligne2; ?>",
-		theme_advanced_buttons3 : "<?php echo $ligne3; ?>",
-		theme_advanced_buttons4 : "<?php echo $ligne4; ?>",
-		theme_advanced_toolbar_location : "<?php echo $bouton; ?>",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "<?php echo $status; ?>",
-		theme_advanced_resizing : true,
+			<script type="text/javascript" src="editeur/tiny_mce.js"></script>
+			<script type="text/javascript">
+				tinyMCE.init({
+					// General options
+					mode : "textareas",
+					theme : "advanced",
+					<?php if ($language == "french") { echo "language : \"fr\",\n"; } ?>
+					plugins : "pagebreak,layer,table,save,advimage,advlink,emotions,spellchecker,inlinepopups,preview,print,contextmenu,paste,directionality,fullscreen,wordcount,advlist,autosave",
+					editor_selector : "editoradvanced",
 
-		// Example content CSS (should be your site CSS)
-		<?php if(is_file('themes/' . $theme . '/editeur.css')) echo 'content_css : "themes/' . $theme. '/editeur.css",'; ?>
+					// Theme options
+					<?php $editeur = array();
+					$sql_editeur = mysql_query("SELECT name, value FROM " . $nuked['prefix'] . "_editeur");
+					while ($row = mysql_fetch_array($sql_editeur))
+					{
+						$editeur[$row['name']] = $row['value'];
+					}
+					unset($sql_editeur, $row); ?>
+					theme_advanced_buttons1 : "<?php echo $editeur['ligne1']; ?>",
+					theme_advanced_buttons2 : "<?php echo $editeur['ligne2']; ?>",
+					theme_advanced_buttons3 : "<?php echo $editeur['ligne3']; ?>",
+					theme_advanced_buttons4 : "<?php echo $editeur['ligne4']; ?>",
+					theme_advanced_toolbar_location : "<?php echo $editeur['bouton']; ?>",
+					theme_advanced_toolbar_align : "left",
+					theme_advanced_statusbar_location : "<?php echo $editeur['status']; ?>",
+					theme_advanced_resizing : true,
 
-		// Drop lists for link/image/media/template dialogs
-		external_link_list_url : "lists/link_list.js",
-		external_image_list_url : "lists/image_list.js",
-		media_external_list_url : "lists/media_list.js",
+					// Example content CSS (should be your site CSS)
+					<?php if(is_file('themes/' . $theme . '/editeur.css')) echo 'content_css : "themes/' . $theme. '/editeur.css",'; ?>
 
-		// Style formats
-		style_formats : [
-			<?php
-			$sql = mysql_query("SELECT texte FROM " . $nuked['prefix'] . "_style ORDER BY id DESC");
-			$nbr = mysql_num_rows($sql);
-			$compteur = 0;
-			while (list($texte) = mysql_fetch_array($sql))
-			{	$compteur++;
-				if($compteur != $nbr)
-				{
-					echo "".$texte.",\n";
-				}
-				else
-				{
-					echo "".$texte."\n";
-				}
-			}
+					// Drop lists for link/image/media/template dialogs
+					external_link_list_url : "lists/link_list.js",
+					external_image_list_url : "lists/image_list.js",
+
+					// Style formats
+					style_formats : [
+					<?php
+					$sql = mysql_query("SELECT texte FROM " . $nuked['prefix'] . "_style ORDER BY id DESC");
+					$nbr = mysql_num_rows($sql);
+					$compteur = 0;
+					while (list($texte) = mysql_fetch_array($sql))
+					{
+						$compteur++;
+
+						if($compteur != $nbr) echo "".$texte.",\n";
+						else echo "".$texte."\n";
+
+					}
+					?>
+					]
+				});
+
+				tinyMCE.init({
+					// General options
+					mode : "textareas",
+					theme : "advanced",
+					<?php if($language == "french") { ?>language : "fr",<?php } ?>
+					plugins : "pagebreak,layer,table,save,advimage,advlink,emotions,spellchecker,inlinepopups,preview,print,contextmenu,paste,directionality,fullscreen,wordcount,advlist,autosave",
+					editor_selector : "editorsimpla",
+
+					// Theme options
+					theme_advanced_buttons1 : "bold,italic,underline,|,justifycenter,|,emotions,forecolor,|,removeformat,|,spellchecker",
+					theme_advanced_buttons2 : "",
+					theme_advanced_buttons3 : "",
+					theme_advanced_buttons4 : "",
+					theme_advanced_toolbar_location : "<?php echo $editeur['bouton']; ?>",
+					theme_advanced_toolbar_align : "left",
+					theme_advanced_statusbar_location : "none",
+					theme_advanced_resizing : true,
+					<?php if(is_file('themes/' . $theme . '/editeur.css')) echo 'content_css : "themes/' . $theme. '/editeur.css",'; ?>
+				});
+			</script>
+
+			<style type="text/css">
+			#forum_texte_toolbargroup { background-color: #<?php echo $editeur['couleur']; ?>; }
+			<?php if (!empty($editeur['couleur']))
+			{ ?>
+			.defaultSkin table.mceLayout tr.mceFirst { font-size: 13px; background-color: #<?php echo $editeur['couleur']; ?>; border: 1px solid #d5d5d5 }
+			.defaultSkin table.mceLayout tr.mceLast { background-color: #<?php echo $editeur['couleur']; ?> }
+			<?php }
+			else if(!empty($bgcolor4))
+			{
 			?>
-			]
-		});
-		tinyMCE.init({
-		// General options
-		mode : "textareas",
-		theme : "advanced",
-		<?php
-		if($language == "french")
-		{
-		?>
-		language : "fr",
-		<?php
+			.defaultSkin table.mceLayout tr.mceFirst { font-size: 13px; background-color: <?php echo $bgcolor4; ?>;	border: 1px solid #d5d5d5 }
+			.defaultSkin table.mceLayout tr.mceLast { background-color: <?php echo $bgcolor4; ?> }
+			<?php
+			} else {
+			?>
+			.defaultSkin table.mceLayout tr.mceFirst { font-size: 13px; background: #fff url('modules/Admin/images/bg-form-field.gif') top left repeat-x; border: 1px solid #d5d5d5 }
+			<?php }
+			echo '</style>';
 		}
-		?>
-		plugins : "pagebreak,layer,table,save,advimage,advlink,emotions,spellchecker,inlinepopups,preview,print,contextmenu,paste,directionality,fullscreen,wordcount,advlist,autosave",
-		editor_selector : "editorsimpla",
-		// Theme options
-		theme_advanced_buttons1 : "bold,italic,underline,|,justifycenter,|,emotions,forecolor,|,removeformat,|,spellchecker",
-		theme_advanced_buttons2 : "",
-		theme_advanced_buttons3 : "",
-		theme_advanced_buttons4 : "",
-		theme_advanced_toolbar_location : "<?php echo $bouton; ?>",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "none",
-		theme_advanced_resizing : true,
-		content_css : "themes/<?php echo $theme; ?>/editeur.css",
-	});
-		</script>
-		<?php
-		$sql = mysql_query("SELECT value FROM " . $nuked['prefix'] . "_editeur WHERE name='couleur'");
-		?>
-		<style type="text/css">
-		#forum_texte_toolbargroup {
-			background-color: #<?php echo mysql_result($sql, 0); ?>;
-		}
-		</style>
-		<?php
-		if ($couleur != "")
-		{
-		?>
-			<style>
-			.defaultSkin table.mceLayout tr.mceFirst
-			{
-			font-size: 13px;
-			background-color: #<?php echo $couleur; ?>;
-			border: 1px solid #d5d5d5;
-			}
-			.defaultSkin table.mceLayout tr.mceLast {
-			background-color: #<?php echo $couleur; ?>;
-			}
-			</style>
-		<?php
-		}
-		else if($bgcolor4 !="")
-		{
-		?>
-			<style>
-			.defaultSkin table.mceLayout tr.mceFirst
-			{
-			font-size: 13px;
-			background-color: <?php echo $bgcolor4; ?>;
-			border: 1px solid #d5d5d5;
-			}
-			.defaultSkin table.mceLayout tr.mceLast {
-			background-color: <?php echo $bgcolor4; ?>;
-			}
-			</style>
-		<!-- /TinyMCE -->
-		<?php
-		}
-		else
-		{
-		?>
-			<style>
-			.defaultSkin table.mceLayout tr.mceFirst
-			{
-			font-size: 13px;
-			background: #fff url('modules/Admin/images/bg-form-field.gif') top left repeat-x;
-			border: 1px solid #d5d5d5;
-			}
-			</style>
-		<?php
-		}
-		}
+		// End TinyMCE Configuration
+
 		$sql = mysql_query('SELECT nom FROM ' . MODULES_TABLE);
 		$signs = array();
 		while($row = mysql_fetch_assoc($sql)){
--- nuked.php
+++ nuked.php
@@ -14,6 +14,8 @@ if (!defined("INDEX_CHECK"))
 
 connect();
 
+date_default_timezone_set(getLocalTimezone());
+
 $nuked = array();
 
 $sql_conf = mysql_query("SELECT name, value FROM " . $db_prefix . "_config");
@@ -74,6 +76,58 @@ if (!isset($_REQUEST['nuked_nude']))
 {
 	if ($language == "french") setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR.ISO8859-1', 'fra', 'france');
 	else setlocale (LC_TIME, $language);
+}
+
+function getLocalTimezone()
+{
+    $iTime = time();
+    $arr = localtime($iTime);
+    $arr[5] += 1900;
+    $arr[4]++;
+    $iTztime = gmmktime($arr[2], $arr[1], $arr[0], $arr[4], $arr[3], $arr[5], $arr[8]);
+    $offset = doubleval(($iTztime-$iTime)/(60*60));
+    $zonelist =
+    array
+    (
+        'Kwajalein' => -12.00,
+        'Pacific/Midway' => -11.00,
+        'Pacific/Honolulu' => -10.00,
+        'America/Anchorage' => -9.00,
+        'America/Los_Angeles' => -8.00,
+        'America/Denver' => -7.00,
+        'America/Tegucigalpa' => -6.00,
+        'America/New_York' => -5.00,
+        'America/Caracas' => -4.30,
+        'America/Halifax' => -4.00,
+        'America/St_Johns' => -3.30,
+        'America/Argentina/Buenos_Aires' => -3.00,
+        'America/Sao_Paulo' => -3.00,
+        'Atlantic/South_Georgia' => -2.00,
+        'Atlantic/Azores' => -1.00,
+        'Europe/Dublin' => 0,
+        'Europe/Belgrade' => 1.00,
+        'Europe/Minsk' => 2.00,
+        'Asia/Kuwait' => 3.00,
+        'Asia/Tehran' => 3.30,
+        'Asia/Muscat' => 4.00,
+        'Asia/Yekaterinburg' => 5.00,
+        'Asia/Kolkata' => 5.30,
+        'Asia/Katmandu' => 5.45,
+        'Asia/Dhaka' => 6.00,
+        'Asia/Rangoon' => 6.30,
+        'Asia/Krasnoyarsk' => 7.00,
+        'Asia/Brunei' => 8.00,
+        'Asia/Seoul' => 9.00,
+        'Australia/Darwin' => 9.30,
+        'Australia/Canberra' => 10.00,
+        'Asia/Magadan' => 11.00,
+        'Pacific/Fiji' => 12.00,
+        'Pacific/Tongatapu' => 13.00
+    );
+    $index = array_keys($zonelist, $offset);
+    if(sizeof($index)!=1)
+        return false;
+    return $index[0];
 }
 
 function session_open($path, $name)
@@ -1247,7 +1301,7 @@ function erreursql($errno, $errstr, $errfile, $errline, $errcontext)
 				exit('Erreur dans la création de la session anonyme');
 			}
 			$date = time();
-			$content = "<b>Veuilliez nous excuser une erreur a été détecté, nous la corrigerons le plus rapidement possible, merci. <br /><br /><b>Information:</b><br /><br /><b>Mon ERREUR</b> [$errno] $errstr<br /> Erreur fatale sur la ligne $errline dans le fichier $errfile, PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />Arrêt...<br />";
+			$content = "<b>Une erreur SQL a été dédecté.<br /><br /><b>Information:</b><br /><br /><b>Mon ERREUR</b> [$errno] $errstr<br /> Erreur fatale sur la ligne $errline dans le fichier $errfile, PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />Arrêt...<br />";
 
 			echo $content;
 
