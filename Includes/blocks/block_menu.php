31d5d5310f5098104c2f6750d1c3f40ae8f1c5d0
Samoth93 <samoth93@gmail.com>
2011-05-13 16:43:09 +0200

    Diverses optimisations performances/Lisibilité (en cours) Part5

--- Includes/blocks/block_menu.php
+++ Includes/blocks/block_menu.php
@@ -9,22 +9,20 @@
 // -------------------------------------------------------------------------//
 if (!defined("INDEX_CHECK")) exit('You can\'t run this file alone.');
 
-function affich_block_menu($blok)
-{
+function affich_block_menu($blok){
     $blok['content'] = block_link($blok['content']);
     return $blok;
 }
 
-function block_link($content)
-{
+function block_link($content){
     global $user;
 
     $link = explode('NEWLINE', $content);
     $screen = '<ul style="list-style: none; padding: 0">';
-
-    foreach ($link as $link)
-    {
-        list($url, $title, $comment, $nivo, $blank) = explode('|', $link);
+	$size = count($link);
+	
+	for($i=0; $i<$size; $i++){
+        list($url, $title, $comment, $nivo, $blank) = explode('|', $link[$i]);
         $url = preg_replace("/\[(.*?)\]/si", "index.php?file=\\1", $url);
         $nivuser = $user[1];
         $title = htmlentities($title, ENT_NOQUOTES);
@@ -34,8 +32,8 @@ function block_link($content)
         $url = htmlentities($url);
 
         if (!$nivuser)$nivuser = 0;
-        if ($nivuser >= $nivo)
-        {
+		
+        if ($nivuser >= $nivo){
             if ($url <> "" && $title <> "" && $blank == 0)
                 $screen .= '<li><a href="' . $url . '" title="' . $comment . '" style="padding-left: 10px" class="menu">' . $title . '</a></li>';
 
@@ -50,8 +48,7 @@ function block_link($content)
     return $screen;
 }
 
-function edit_block_menu($bid)
-{
+function edit_block_menu($bid){
     global $nuked, $language;
 
     $sql = mysql_query("SELECT active, position, titre, module, content, type, nivo, page FROM " . BLOCK_TABLE . " WHERE bid = '" . $bid . "'");
@@ -60,49 +57,48 @@ function edit_block_menu($bid)
     $titre = htmlentities($titre);
     $content = htmlentities($content);
 
-    if ($active == 1) $checked1 = "selected=\"selected\"";
-    else if ($active == 2) $checked2 = "selected=\"selected\"";
-    else $checked0 = "selected=\"selected\"";
+    if ($active == 1) $checked1 = 'selected="selected"';
+    else if ($active == 2) $checked2 = 'selected="selected"';
+    else $checked0 = 'selected="selected"';
 
-    echo "<div class=\"content-box\">\n" //<!-- Start Content Box -->
-		. "<div class=\"content-box-header\"><h3>" . _BLOCKADMIN . "</h3>\n"
-		. "<a href=\"help/" . $language . "/block.html\" rel=\"modal\">\n"
-	. "<img style=\"border: 0;\" src=\"help/help.gif\" alt=\"\" title=\"" . _HELP . "\" /></a>\n"
-	. "</div>\n"
-	. "<div class=\"tab-content\" id=\"tab2\"><form method=\"post\" action=\"index.php?file=Admin&amp;page=block&amp;op=modif_block\">\n"
-    . "<table style=\"margin-left: auto;margin-right: auto;text-align: left;\" cellspacing=\"0\" cellpadding=\"2\" border=\"0\">\n"
-    . "<tr><td><b>" . _TITLE . "</b></td><td><b>" . _BLOCK . "</b></td><td><b>" . _POSITION . "</b></td><td><b>" . _LEVEL . "</b></td></tr>\n"
-    . "<tr><td align=\"center\"><input type=\"text\" name=\"titre\" size=\"40\" value=\"" . $titre . "\" /></td>\n"
-    . "<td align=\"center\"><select name=\"active\">\n"
-    . "<option value=\"1\" " . $checked1 . ">" . _LEFT . "</option>\n"
-    . "<option value=\"2\" " . $checked2 . ">" . _RIGHT . "</option>\n"
-    . "<option value=\"0\" " . $checked0 . ">" . _OFF . "</option></select></td>\n"
-    . "<td align=\"center\"><input type=\"text\" name=\"position\" size=\"2\" value=\"" . $position . "\" /></td>\n"
-    . "<td align=\"center\"><select name=\"nivo\"><option>" . $nivo . "</option>\n"
-    . "<option>0</option>\n"
-    . "<option>1</option>\n"
-    . "<option>2</option>\n"
-    . "<option>3</option>\n"
-    . "<option>4</option>\n"
-    . "<option>5</option>\n"
-    . "<option>6</option>\n"
-    . "<option>7</option>\n"
-    . "<option>8</option>\n"
-    . "<option>9</option></select></td></tr>\n"
-    . "<tr><td colspan=\"4\" align=\"center\"><br /><input type=\"button\" value=\"" . _EDITMENU . "\" onclick=\"javascript:window.location='index.php?file=Admin&amp;page=menu&amp;op=edit_menu&amp;bid=" . $bid . "'\" /></td></tr>\n"
-    . "<tr><td colspan=\"4\">&nbsp;</td></tr><tr><td colspan=\"4\" align=\"center\"><b>" . _PAGESELECT . " :</b></td></tr><tr><td colspan=\"4\">&nbsp;</td></tr>\n"
-    . "<tr><td colspan=\"4\" align=\"center\"><select name=\"pages[]\" size=\"8\" multiple=\"multiple\">\n";
+    echo '<div class="content-box">',"\n" //<!-- Start Content Box -->
+		, '<div class="content-box-header"><h3>' , _BLOCKADMIN , '</h3>',"\n"
+		, '<a href="help/' , $language , '/block.html" rel="modal">',"\n"
+		, '<img style="border: 0;" src="help/help.gif" alt="" title="' , _HELP , '" /></a>',"\n"
+		, '</div>',"\n"
+		, '<div class="tab-content" id="tab2"><form method="post" action="index.php?file=Admin&amp;page=block&amp;op=modif_block">',"\n"
+		, '<table style="margin-left: auto;margin-right: auto;text-align: left;" cellspacing="0" cellpadding="2" border="0">',"\n"
+		, '<tr><td><b>' , _TITLE , '</b></td><td><b>' , _BLOCK , '</b></td><td><b>' , _POSITION , '</b></td><td><b>' , _LEVEL , '</b></td></tr>',"\n"
+		, '<tr><td align="center"><input type="text" name="titre" size="40" value="' , $titre , '" /></td>',"\n"
+		, '<td align="center"><select name="active">',"\n"
+		, '<option value="1" ' , $checked1 , '>' , _LEFT , '</option>',"\n"
+		, '<option value="2" ' , $checked2 , '>' , _RIGHT , '</option>',"\n"
+		, '<option value="0" ' , $checked0 , '>' , _OFF , '</option></select></td>',"\n"
+		, '<td align="center"><input type="text" name="position" size="2" value="' , $position , '" /></td>',"\n"
+		, '<td align="center"><select name="nivo"><option>' , $nivo , '</option>',"\n"
+		, '<option>0</option>',"\n"
+		, '<option>1</option>',"\n"
+		, '<option>2</option>',"\n"
+		, '<option>3</option>',"\n"
+		, '<option>4</option>',"\n"
+		, '<option>5</option>',"\n"
+		, '<option>6</option>',"\n"
+		, '<option>7</option>',"\n"
+		, '<option>8</option>',"\n"
+		, '<option>9</option></select></td></tr>',"\n"
+		, '<tr><td colspan="4" align="center"><br /><input type="button" value="' , _EDITMENU , '" onclick="javascript:window.location='index.php?file=Admin&amp;page=menu&amp;op=edit_menu&amp;bid=' , $bid , ''" /></td></tr>',"\n"
+		, '<tr><td colspan="4">&nbsp;</td></tr><tr><td colspan="4" align="center"><b>' , _PAGESELECT , ' :</b></td></tr><tr><td colspan="4">&nbsp;</td></tr>',"\n"
+		, '<tr><td colspan="4" align="center"><select name="pages[]" size="8" multiple="multiple">',"\n";
 
     select_mod2($pages);
 
-    echo "</select></td></tr><tr><td colspan=\"4\" align=\"center\"><br />\n"
-    . "<input type=\"hidden\" name=\"type\" value=\"" . $type . "\" />\n"
-    . "<input type=\"hidden\" name=\"bid\" value=\"" . $bid . "\" />\n"
-    . "<input type=\"hidden\" name=\"content\" value=\"$content\" />\n"
-    . "<input type=\"submit\" name=\"send\" value=\"" . _MODIFBLOCK . "\" />\n"
-    . "</td></tr></table>\n"
-    . "<div style=\"text-align: center;\"><br />[ <a href=\"index.php?file=Admin&amp;page=block\"><b>" . _BACK . "</b></a> ]</div></form><br /></div></div>\n";
+    echo '</select></td></tr><tr><td colspan="4" align="center"><br />',"\n"
+    , '<input type="hidden" name="type" value="' , $type , '" />',"\n"
+    , '<input type="hidden" name="bid" value="' , $bid , '" />',"\n"
+    , '<input type="hidden" name="content" value="$content" />',"\n"
+    , '<input type="submit" name="send" value="' , _MODIFBLOCK , '" />',"\n"
+    , '</td></tr></table>',"\n"
+    , '<div style="text-align: center;"><br />[ <a href="index.php?file=Admin&amp;page=block"><b>' , _BACK , '</b></a> ]</div></form><br /></div></div>',"\n";
 
 }
-
 ?>
