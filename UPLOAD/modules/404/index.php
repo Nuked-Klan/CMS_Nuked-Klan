481eb415955b532d07f287e98e23c509206c0147
sekuline <sekuline@gmail.com>
2011-06-10 04:52:55 +0200

    Correction du module 404 pour l'Administration

--- UPLOAD/modules/404/index.php
+++ UPLOAD/modules/404/index.php
@@ -13,10 +13,19 @@ global $nuked, $language;
 translate('modules/Search/lang/' . $language . '.lang.php');
 translate('modules/404/lang/' . $language . '.lang.php');
 
-opentable();
+if($_REQUEST['file'] == 'Admin' && $_REQUEST['page'] == 'admin') {
+	$opentable = opentable();
+	$closetable = closetable();
+} else {
+	include('modules/Admin/design.php');
+	$opentable = admintop();
+	$closetable = adminfoot();
+}
 
-if($_REQUEST['op'] != 'sql') $error_title = '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />';
-else $error_title = _ERROR404SQL . '<br /><br />';
+	print $opentable;
+
+	if($_REQUEST['op'] != 'sql') $error_title = '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />';
+	else $error_title = _ERROR404SQL . '<br /><br />';
 
     echo '<div style="text-align: center; padding: 0 10px">' . $error_title . '
     <form method="post" action="index.php?file=Search&amp;op=mod_search">
@@ -24,5 +33,5 @@ else $error_title = _ERROR404SQL . '<br /><br />';
     <p><input type="submit" class="button" name="submit" value="' . _SEARCHFOR . '" /></p>
     <p><a href="index.php?file=Search"><b>' . _ADVANCEDSEARCH . '</b></a> - <a href="javascript:history.back()"><b>' . _BACK . '</b></a></form></div>';
 
-closetable();
+	print $closetable();
 ?>
