7d3ced63d80e2fc7cdb17601341081cd65274b5e
H <H@D3S>
2011-06-13 22:28:28 +0200

    Réupload du fichier coriger par Samoth

--- UPLOAD/Includes/blocks/block_event.php
+++ UPLOAD/Includes/blocks/block_event.php
@@ -64,14 +64,14 @@ function affich_block_event($blok){
     $this_month = $month - 1;
     $days = Array(_SUN, _MON, _TUE, _WEN, _THR, _FRI, _SAT);
 
-    $blok['content'] ,= '<table style="margin-left: auto;margin-right: auto;text-align: left;" cellpadding="0" cellspacing="0"><tr><td>',"\n"
-							, '<a href="index,php?file=' , $file , '&amp;mo=' , $prevmonth , '&amp;ye=' , $prevyear , '" title="' , _PREVMONTH , '"><small>&lt;&lt;</small></a>&nbsp;<b>' , $months[$this_month] , '&nbsp;' , $year , '</b>&nbsp;',"\n"
-							, '<a href="index,php?file=' , $file , '&amp;mo=' , $nextmonth , '&amp;ye=' , $nextyear , '" title="' , _NEXTMONTH , '"><small>&gt;&gt;</small></a></td></tr></table>',"\n"
-							, '<table style="margin-left: auto;margin-right: auto;text-align: left;" cellpadding="2" cellspacing="1"><tr>',"\n";
+    $blok['content'] .= '<table style="margin:0 auto;text-align:left" cellpadding="0" cellspacing="0"><tr><td>'."\n"
+					 . '<a href="index.php?file='.$file.'&amp;mo=' , $prevmonth . '&amp;ye='.$prevyear.'" title="'._PREVMONTH.'"><small>&lt;&lt;</small></a>&nbsp;<b>'.$months[$this_month].'&nbsp;'.$year.'</b>&nbsp;',"\n"
+					 . '<a href="index,php?file='.$file.'&amp;mo='.$nextmonth.'&amp;ye='.$nextyear.'" title="'._NEXTMONTH.'"><small>&gt;&gt;</small></a></td></tr></table>'."\n"
+					 . '<table style="margin:0 auto;text-align:left" cellpadding="2" cellspacing="1"><tr>'."\n";
 
 	$size = count($days);
 	for($i=0; $i<$size; $i++){
-		$blok['content'] .= '<td style="text-align:center;><b>' . $days[$i] . '</b></td>';
+		$blok['content'] .= '<td style="text-align:center"><b>' . $days[$i] . '</b></td>';
 	}
 
     for($count = 0;$count < (6 * 7);$count++){
@@ -258,41 +258,41 @@ function edit_block_event($bid){
     else if ($active == 2) $checked2 = 'selected="selected"';
     else $checked0 = 'selected="selected"';
 
-    echo '<div class="content-box">',"\n" //<!-- Start Content Box -->
-			, '<div class="content-box-header"><h3>' , _BLOCKADMIN , '</h3>',"\n"
-			, '<a href="help/' , $language , '/block,html" rel="modal">',"\n"
-			, '<img style="border: 0;" src="help/help,gif" alt="" title="' , _HELP , '" /></a>',"\n"
-			, '</div>',"\n"
-			, '<div class="tab-content" id="tab2"><form method="post" action="index,php?file=Admin&amp;page=block&amp;op=modif_block">',"\n"
-			, '<table style="margin-left: auto;margin-right: auto;text-align: left;" cellspacing="0" cellpadding="2" border="0">',"\n"
-			, '<tr><td><b>' , _TITLE , '</b></td><td><b>' , _BLOCK , '</b></td><td><b>' , _POSITION , '</b></td><td><b>' , _LEVEL , '</b></td></tr>',"\n"
-			, '<tr><td style="text-align:center;" ><input type="text" name="titre" size="40" value="' , $titre , '" /></td>',"\n"
-			, '<td style="text-align:center;"><select name="active">',"\n"
-			, '<option value="1" ' , $checked1 , '>' , _LEFT , '</option>',"\n"
-			, '<option value="2" ' , $checked2 , '>' , _RIGHT , '</option>',"\n"
-			, '<option value="0" ' , $checked0 , '>' , _OFF , '</option></select></td>',"\n"
-			, '<td style="text-align:center;" ><input type="text" name="position" size="2" value="' , $position , '" /></td>',"\n"
-			, '<td style="text-align:center;" ><select name="nivo"><option>' , $nivo , '</option>',"\n"
-			, '<option>0</option>',"\n"
-			, '<option>1</option>',"\n"
-			, '<option>2</option>',"\n"
-			, '<option>3</option>',"\n"
-			, '<option>4</option>',"\n"
-			, '<option>5</option>',"\n"
-			, '<option>6</option>',"\n"
-			, '<option>7</option>',"\n"
-			, '<option>8</option>',"\n"
-			, '<option>9</option></select></td></tr><tr><td colspan="4">&nbsp;</td></tr><tr><td colspan="4" style="text-align:center;" ><b>' , _PAGESELECT , ' :</b></td></tr><tr><td colspan="4">&nbsp;</td></tr>',"\n"
-			, '<tr><td colspan="4" align="center"><select name="pages[]" size="8" multiple="multiple">',"\n";
+    echo '<div class="content-box">'."\n" //<!-- Start Content Box -->
+	   . '<div class="content-box-header"><h3>'._BLOCKADMIN.'</h3>'."\n"
+	   . '<a href="help/'.$language.'/block.html" rel="modal">'."\n"
+	   . '<img style="border: 0;" src="help/help.gif" alt="" title="'._HELP.'" /></a>'."\n"
+	   . '</div>'."\n"
+	   . '<div class="tab-content" id="tab2"><form method="post" action="index.php?file=Admin&amp;page=block&amp;op=modif_block">'."\n"
+	   . '<table style="margin:0 auto;text-align: left;" cellspacing="0" cellpadding="2" border="0">'."\n"
+	   . '<tr><td><b>'._TITLE.'</b></td><td><b>'._BLOCK.'</b></td><td><b>'._POSITION.'</b></td><td><b>'._LEVEL.'</b></td></tr>'."\n"
+	   . '<tr><td style="text-align:center;" ><input type="text" name="titre" size="40" value="'.$titre.'" /></td>'."\n"
+	   . '<td style="text-align:center;"><select name="active">'."\n"
+	   . '<option value="1" '.$checked1.'>'._LEFT.'</option>',"\n"
+	   . '<option value="2" '.$checked2.'>'._RIGHT.'</option>',"\n"
+	   . '<option value="0" '.$checked0.'>'._OFF.'</option></select></td>'."\n"
+	   . '<td style="text-align:center;" ><input type="text" name="position" size="2" value="'.$position.'" /></td>'."\n"
+	   . '<td style="text-align:center;" ><select name="nivo"><option>'.$nivo.'</option>'."\n"
+	   . '<option>0</option>'."\n"
+	   . '<option>1</option>'."\n"
+	   . '<option>2</option>'."\n"
+	   . '<option>3</option>'."\n"
+	   . '<option>4</option>'."\n"
+	   . '<option>5</option>'."\n"
+	   . '<option>6</option>'."\n"
+	   . '<option>7</option>'."\n"
+	   . '<option>8</option>'."\n"
+	   . '<option>9</option></select></td></tr><tr><td colspan="4">&nbsp;</td></tr><tr><td colspan="4" style="text-align:center;" ><b>'._PAGESELECT.' :</b></td></tr><tr><td colspan="4">&nbsp;</td></tr>'."\n"
+	   . '<tr><td colspan="4" align="center"><select name="pages[]" size="8" multiple="multiple">'."\n";
 
     select_mod2($pages);
 
-    echo '</select></td></tr><tr><td colspan="4" style="text-align:center;" ><br />',"\n"
-			, '<input type="hidden" name="type" value="' , $type , '" />',"\n"
-			, '<input type="hidden" name="bid" value="' , $bid , '" />',"n"
-			, '<input type="submit" name="send" value="' , _MODIFBLOCK , '" />',"\n"
-			, '</td></tr></table>'
-			, '<div style="text-align: center;"><br />[ <a href="index,php?file=Admin&amp;page=block"><b>' , _BACK , '</b></a> ]</div></form><br /></div></div>',"\n";
+    echo '</select></td></tr><tr><td colspan="4" style="text-align:center;" ><br />'."\n"
+	   . '<input type="hidden" name="type" value="'.$type.'" />'."\n"
+	   . '<input type="hidden" name="bid" value="'.$bid.'" />'."n"
+	   . '<input type="submit" name="send" value="'._MODIFBLOCK.'" />'."\n"
+	   . '</td></tr></table>'
+	   . '<div style="text-align: center;"><br />[ <a href="index,php?file=Admin&amp;page=block"><b>'._BACK.'</b></a> ]</div></form><br /></div></div>'."\n";
 
 }
 ?>
