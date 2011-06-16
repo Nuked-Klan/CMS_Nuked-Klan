ec1d77b6cc4454c7c9779052c210d55318d25dc8
H <H@D3S>
2011-06-16 13:59:04 +0200

    Re-correction sur module 404 du fichier index.php

--- UPLOAD/modules/404/index.php
+++ UPLOAD/modules/404/index.php
@@ -13,25 +13,26 @@ global $nuked, $language;
 translate('modules/Search/lang/' . $language . '.lang.php');
 translate('modules/404/lang/' . $language . '.lang.php');
 
-if($_REQUEST['file'] == 'Admin' && $_REQUEST['page'] == 'admin') {
-	$opentable = opentable();
-	$closetable = closetable();
-} else {
+if($_REQUEST['file'] == 'Admin'){
 	include('modules/Admin/design.php');
 	$opentable = admintop();
 	$closetable = adminfoot();
+}else{
+	$opentable = opentable();
+	$closetable = closetable();
 }
 
-	print $opentable;
+print $opentable;
 
-	if($_REQUEST['op'] != 'sql') $error_title = '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />';
-	else $error_title = _ERROR404SQL . '<br /><br />';
+$error_title = ($_REQUEST['op'] != 'sql') ? '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />' : _ERROR404SQL . '<br /><br />';
 
-    echo '<div style="text-align: center; padding: 0 10px">' . $error_title . '
-    <form method="post" action="index.php?file=Search&amp;op=mod_search">
-    <p><input type="hidden" name="module" value="" /><input type="text" name="main" size="25" /></p>
-    <p><input type="submit" class="button" name="submit" value="' . _SEARCHFOR . '" /></p>
-    <p><a href="index.php?file=Search"><b>' . _ADVANCEDSEARCH . '</b></a> - <a href="javascript:history.back()"><b>' . _BACK . '</b></a></form></div>';
+echo '<div style="text-align: center; padding: 0 10px">' . $error_title . '
+          <form method="post" action="index.php?file=Search&amp;op=mod_search">
+              <p><input type="hidden" name="module" value="" /><input type="text" name="main" size="25" /></p>
+              <p><input type="submit" class="button" name="submit" value="' . _SEARCHFOR . '" /></p>
+              <p><a href="index.php?file=Search"><b>' . _ADVANCEDSEARCH . '</b></a> - <a href="javascript:history.back()"><b>' . _BACK . '</b></a>
+		  </form>
+	  </div>';
 
-	print $closetable;
+print $closetable;
 ?>
