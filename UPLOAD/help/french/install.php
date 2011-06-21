14a2490c796c512900621096e1313c3dc94b61fc
akred <akred@live.com>
2011-06-21 21:24:16 +0200

    Correction bad commit r347

--- UPLOAD/help/french/install.php
+++ UPLOAD/help/french/install.php
@@ -10,19 +10,19 @@
 <li><b>Host MySQL</b> : L'adresse de votre serveur MySQL, c'est dans la plupart des cas <b><i>localhost</i></b> quelques exceptions courantes :<br>
 <b><i>Free.fr :</b></i> sql.free.fr<br>
 <b><i>HLexpert</i> :</b> mysqlbox<br>
-Autre cas consultez l'aide de votre hebergeur.<br>
+Autre cas consultez l'aide de votre hébergeur.<br>
 <br>
 <li><b>User</b> : C'est le login qui vous permet de vous connecter à la base MySQL, il est presque toujours le même que votre login FTP.<br>
 <br>
 <li><b>Password</b> : C'est le mot de passe qui vous permet de vous connecter à la base MySQL,  il est presque toujours le même que votre mot de passe FTP.<br>
 <br>
-<li><b>Prefix</b> :  C'est le préfixe des tables sql, vous pouvez mettre ce que vous voulez, sauf dans le cas d'une update.<br>
+<li><b>Prefix</b> :  C'est le préfixe des tables SQL, vous pouvez mettre ce que vous voulez, sauf dans le cas d'une update.<br>
 <br>
-<li><b>Nom de la Base</b> : C'est le nom de la base MySQL, il est preque toujours le même que votre login FTP.<br> 
+<li><b>Nom de la Base</b> : C'est le nom de la base MySQL, il est presque toujours le même que votre login FTP.<br> 
 <br>
 <br>
 <br>
 <br>
 <i><b>Note :</b> N'oubliez pas de modifier les attributs des fichiers conf.inc.php et install.php (ou update.php) en chmod666 avant
-de lancer l'installation. Plus d'infos sur les CHMOD sur <a href=http://www.phpdebutant.org/article50.php target=_blank>PHP Débutant</a></i><br>
+de lancer l'installation. Plus d'infos sur les CHMOD sur <a href="http://www.phpdebutant.org/article50.php" target="_blank">PHP Débutant</a></i><br>
 </body></html>
