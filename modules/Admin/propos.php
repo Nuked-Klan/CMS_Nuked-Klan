dd93dd0ba14c94cdcf0b6696941f5dbad581d079
Sekuline <sekuline@gmail.com>
2011-12-14 14:40:44 +0100

    Mise à jour de la page a propos.

--- modules/Admin/propos.php
+++ modules/Admin/propos.php
@@ -7,7 +7,7 @@
 // it under the terms of the GNU General Public License as published by     //
 // the Free Software Foundation; either version 2 of the License.           //
 // -------------------------------------------------------------------------//
-defined('INDEX_CHECK') or die ('You can\'t run this file alone.');
+defined('INDEX_CHECK') or die;
 
 global $user, $language;
 translate('modules/Admin/lang/' . $language . '.lang.php');
@@ -15,55 +15,44 @@ include('modules/Admin/design.php');
 
 $visiteur = $user ? $user[1] : 0;
 
-if ($visiteur >= 2)
-{
-    admintop();
+admintop();
+
+if ($visiteur >= 2) {
     ?>
     <div class="content-box"><!-- Start Content Box -->
-    <div class="content-box-header"><h3><?php echo _PROPOS; ?></h3></div>
-    <div class="tab-content" id="tab2"><div style="margin:20px">
-    <h3>Informations générales :</h3>
-    Version <?php echo $nuked['version']; ?><br />
-    Développée par toute l'équipe Nuked-KlaN<br />
-    Développement géré par : fce.<br />
-    Développeurs : bontiv, fce, xpLosIve, oeildefeu, Akred, Samoth, Sekuline, PooG, H@D3S, BaHaMuT.<br />
-    Responsables Qualité : Lypso21, Zdav<br /><br /><br />
-    
-    <h3>Contact Nuked-KlaN :</h3>
-    <a href="http://www.nuked-klan.org/index.php?file=Contact">Formulaire de contact</a><br />
-    Administrateur : fce@nuked-klan.org<br />
-    Community Manager : bankai et perperpervers<br />
-    Responsable : oobahamutoo@nuked-klan.org<br /><br /><br />
-    
-    <h3>Remerciements:</h3>
-    A toute l'équipe nuked-klan.org, ainsi qu'à sa communauté qui nous a permis de corriger rapidement les principaux bugs<br /><br /><br />
-    
-    <h3>Licence GNU:</h3>
-    Merci de ne pas supprimer le copyleft par respect pour le cms et respecter la licence GNU.<br />
-    </div>
-    <div style="text-align: center"><br />[ <a href="index.php?file=Admin"><b><?php echo _BACK; ?></b></a> ]</div><br /></div></div>
-    <?php
-    adminfoot();
+        <div class="content-box-header"><h3><?php echo _PROPOS; ?></h3></div>
+        <div class="tab-content" id="tab2">
+            <div style="margin:20px">
+                <h3>Informations générales :</h3>
+                Version <?php echo $nuked['version']; ?><br />
+                Développée par toute <a href="http://www.nuked-klan.org/index.php?file=Equipe">l'équipe Nuked-KlaN</a><br />
 
+                <h3>Contact Nuked-KlaN :</h3>
+                <a href="http://www.nuked-klan.org/index.php?file=Contact">Formulaire de contact</a><br />
+
+                <h3>Remerciements:</h3>
+                A toute l'équipe nuked-klan.org, ainsi qu'à sa communauté qui nous a permis de corriger rapidement les principaux bugs<br /><br /><br />
+
+                <h3>Licence GNU:</h3>
+                Merci de ne pas supprimer le <acronym title="Le terme copyleft est un double jeu de mots faisant référence au copyright traditionnel">copyleft</acronym> par respect pour le cms et respecter la licence GNU.<br />
+            </div>
+            <div style="text-align: center"><br />[ <a href="index.php?file=Admin"><b><?php echo _BACK; ?></b></a> ]<br /></div>
+        </div>
+    </div>
+    <?php
 }
-else if ($visiteur > 1)
-{
-    admintop();
+else if ($visiteur > 1) {
     echo "<div class=\"notification error png_bg\">\n"
     . "<div>\n"
     . "<br /><br /><div style=\"text-align: center;\">" . _NOENTRANCE . "<br /><br /><a href=\"javascript:history.back()\"><b>" . _BACK . "</b></a></div><br /><br />"
     . "</div>\n"
     . "</div>\n";
-    adminfoot();
 }
-else
-{
-    admintop();
+else {
     echo "<div class=\"notification error png_bg\">\n"
     . "<div>\n"
     . "<br /><br /><div style=\"text-align: center;\">" . _ZONEADMIN . "<br /><br /><a href=\"javascript:history.back()\"><b>" . _BACK . "</b></a></div><br /><br />"
     . "</div>\n"
     . "</div>\n";
-    adminfoot();
 }
 ?>
