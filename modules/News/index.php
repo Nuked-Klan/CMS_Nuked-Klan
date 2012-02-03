e577657fd297912198ba11588d4472da4137495f
sekuline <sekuline@gmail.com>
2012-02-03 01:03:30 +0100

    News : Ajout d'un nouveau variable pour ne récuperer que le timespan de la date

--- modules/News/index.php
+++ modules/News/index.php
@@ -80,6 +80,7 @@ if ($visiteur >= $level_access && $level_access > -1) {
             else $auteur = $TabNews['auteur'];
             
             $data['date'] = nkDate($TabNews['date']);
+            $data['date_timespan'] = $TabNews['date']);
             $data['cat'] = $TabCat['titre'];
             $data['catid'] = $TabNews['cat'];
             $data['id'] = $TabNews['id'];
