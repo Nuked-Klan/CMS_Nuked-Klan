9c28139bb915594a980b9427350429679fa1ba93
Samoth <samoth93@gmail.com>
2013-06-12 03:48:43 +0200

    Fix php errors in Members and Search Mod

--- modules/Search/index.php
+++ modules/Search/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // -------------------------------------------------------------------------//
 // Nuked-KlaN - PHP Portal                                                  //
 // http://www.nuked-klan.org                                                //
@@ -9,7 +9,7 @@
 // -------------------------------------------------------------------------//
 if (!defined("INDEX_CHECK")){
     die ("<div style=\"text-align: center;\">You cannot open this page directly</div>");
-} 
+}
 
 global $nuked, $language;
 
@@ -20,8 +20,18 @@ opentable();
 function index(){
     global $nuked, $user, $bgcolor1, $bgcolor2, $bgcolor3;
 
+    $arrayRequest = array('module', 'main', 'autor', 'limit', 'searchtype');
+
+    foreach($arrayRequest as $key){
+        if(!array_key_exists($key, $_REQUEST)){
+            $_REQUEST[$key] = '';
+        }
+    }
+
     $_REQUEST['main'] = stripslashes($_REQUEST['main']);
-    
+
+    $checked1 = $checked2 = $checked3 = $checked4 = $checked5 = $checked6 = '';
+
     if ($_REQUEST['searchtype'] == "matchor") $checked1 = "checked=\"checked\"";
     else if ($_REQUEST['searchtype'] == "matchexact") $checked3 = "checked=\"checked\"";
     else $checked2 = "checked=\"checked\"";
@@ -44,26 +54,27 @@ function index(){
     $path = "modules/Search/rubriques/";
     $modules = array();
     $handle = opendir($path);
-	
+
+    $i = 0;
     while ($mod = readdir($handle)){
         if ($mod != "." && $mod != ".." && $mod != "index.html"){
             $i++;
             $mod = str_replace(".php", "", $mod);
             $perm = nivo_mod($mod);
             if (!$perm) $perm = 0;
-			
+
             if ($user[1] >= $perm && $perm > -1){
                 $umod = strtoupper($mod);
                 $modname = "_S" . $umod;
                 if (defined($modname)) $modname = constant($modname);
                 else $modname = $mod;
     	        array_push($modules, $modname . "|" . $mod);
-            } 
-        } 
+            }
+        }
     }
-	
+
     natcasesort($modules);
-	
+
     foreach($modules as $value){
 		$temp = explode("|", $value);
 		if ($temp[1] == $_REQUEST['module']) $selected = "selected=\"selected\"";
@@ -79,8 +90,19 @@ function index(){
 			. "<input type=\"submit\" name=\"submit\" value=\"" . _SEARCHFOR . "\" /></td></tr></table></form><br />\n";
 }
 
-function mod_search($module, $main, $autor, $limit, $searchtype){
+function mod_search(){
     global $nuked, $user, $bgcolor1, $bgcolor2, $bgcolor3;
+
+    $arrayRequest = array('module', 'main', 'autor', 'limit', 'searchtype');
+
+    foreach($arrayRequest as $key){
+        if(array_key_exists($key, $_REQUEST)){
+            ${$key} = $_REQUEST[$key];
+        }
+        else{
+            ${$key} = '';
+        }
+    }
 
     if (preg_match("`%20union%20`i", $main) ||preg_match("`union`i ", $main) || preg_match("`\*union\*`i", $main) || preg_match("`\+union\+`i", $main) || preg_match("`\*`i", $main)){
 		echo "<br /><br /><div style=\"text-align: center;\"><big>What are you trying to do ?</big></div><br /><br />";
@@ -88,7 +110,7 @@ function mod_search($module, $main, $autor, $limit, $searchtype){
 		closetable();
 		footer();
 		exit();
-    }	
+    }
 
     if (!$limit) $limit = 50;
     if (!$searchtype) $searchtype = "matchand";
@@ -106,7 +128,7 @@ function mod_search($module, $main, $autor, $limit, $searchtype){
             closetable();
             footer();
             exit();
-        } 
+        }
 
         $main = mysql_real_escape_string(stripslashes($main));
         $autor = htmlentities($autor, ENT_QUOTES, 'ISO-8859-1');
@@ -117,69 +139,75 @@ function mod_search($module, $main, $autor, $limit, $searchtype){
 
         $path = "modules/Search/rubriques/";
         $handle = opendir($path);
-		
+
         while ($mod = readdir($handle)){
             if ($mod != "." && $mod != ".." && $mod != "index.html"){
                 $i++;
                 $mod = str_replace(".php", "", $mod);
                 $perm = nivo_mod($mod);
                 if (!$perm) $perm = 0;
-				
+
                 if ($user[1] >= $perm && $perm > -1 && ($module == $mod || $module == "")){
                     $umod = strtoupper($mod);
                     $modname = "_S" . $umod;
                     if (defined($modname)) $modname = constant($modname);
                     else $modname = $mod;
                     require("modules/Search/rubriques/" . $mod . ".php");
-                } 
-            } 
-        } 
+                }
+            }
+        }
         $l = count($tab['module']);
 
-        if (!$_REQUEST['p']) $_REQUEST['p'] = 1;
-        $start = $_REQUEST['p'] * $limit - $limit;
+        if(array_key_exists('p', $_REQUEST)){
+            $page = $_REQUEST['p'];
+        }
+        else{
+            $page = 1;
+        }
+        $start = $page * $limit - $limit;
         $end = $start + $limit;
         if ($end > $l) $end = $l;
 
         echo "<div style=\"text-align: center;\"><b>" . _RETURN . "&nbsp;" . $l . "&nbsp;" . _RESULTS . "</b></div><br />\n";
 
-        if ($l > $limit){ 
+        if ($l > $limit){
 			$search_url = "index.php?file=Search&amp;op=mod_search&amp;main=" . urlencode($main) . "&amp;autor=" . $autor . "&amp;module=" . $module . "&amp;limit=" . $limit . "&amp;searchtype=" . $searchtype;
 			number($l, $limit, $search_url);
 		}
 
         echo "<table style=\"background: " . $bgcolor2 . ";border: 1px solid " . $bgcolor3 . ";\" width=\"100%\" cellspacing=\"2\" cellpadding=\"3\" border=\"0\">\n";
 
+        $z = 0;
         for($a = $start;$a < $end;$a++){
             if ($z == 0){
                 $bg = $bgcolor2;
                 $z++;
-            } 
+            }
             else{
                 $bg = $bgcolor1;
                 $z = 0;
             }
-			
+
             echo "<tr style=\"background: " . $bg . ";\"><td width=\"100%\"><a href=\"" . $tab['link'][$a] . "\">" . $tab['title'][$a] . "</a></td><td><big>" . $tab['module'][$a] . "</big></td></tr>\n";
-        } 
+        }
 
         echo "</table>\n";
 
-        if ($l > $limit){ 
+        if ($l > $limit){
 			$search_url = "index.php?file=Search&amp;op=mod_search&amp;main=" . urlencode($main) . "&amp;autor=" . $autor . "&amp;module=" . $module . "&amp;limit=" . $limit . "&amp;searchtype=" . $searchtype;
 			number($l, $limit, $search_url);
 		}
 
 		echo"<br />";
-    } 
+    }
     else{
         echo "<div style=\"text-align: center;\"><br />" . _NOWORDS . "</div><br /><br />\n";
-    } 
-} 
+    }
+}
 
 switch ($_REQUEST['op']){
     case "mod_search":
-    mod_search($_REQUEST['module'], $_REQUEST['main'], $_REQUEST['autor'], $_REQUEST['limit'], $_REQUEST['searchtype']);
+    mod_search();
     break;
 
     default:
