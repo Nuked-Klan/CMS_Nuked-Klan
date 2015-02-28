0e67e53d5edf2e9e4e54bff225485bbd1b2c6e96
Samoth <samoth93@gmail.com>
2015-02-28 02:45:54 +0100

    Fix twitter name var + string

--- modules/Team/lang/french.lang.php
+++ modules/Team/lang/french.lang.php
@@ -14,7 +14,7 @@ define("_XFIRE","Xfire");
 define("_FACEBOOK","FaceBook");
 define("_ORIGINEA","Origin");
 define("_STEAM","Steam");
-define("_TWITER","Twiter");
+define("_TWITTER","Twitter");
 define("_SKYPE","Skype");
 define("_RANK","Rang");
 define("_VIEWPROFIL","Voir le profil");
