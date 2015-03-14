da03901bad3e03f1f39c9649eed3015888c0f69e
Zdav <zdav34@gmail.com>
2015-03-14 16:27:50 +0100

    Fix admin shorcut menu display

--- modules/Links/menu/english/menu.php
+++ modules/Links/menu/english/menu.php
@@ -2,33 +2,40 @@
 header('Content-type: text/html; charset=iso-8859-1');
 ?>
 <html><body>
-				<li><a class="shortcut-button" href="index.php?file=Links&amp;page=admin"><span>
-					<img src="modules/Admin/images/icons/home.png" alt="icon" /><br />
-					Manage the module
-				</span></a></li>
-				
-				<li><a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=add_link"><span>
-					<img src="modules/Admin/images/icons/pencil_48.png" alt="icon" /><br />
-					Add a link
-				<br />&nbsp;</span></a></li>
-				
-				<li><a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=main_cat"><span>
-					<img src="modules/Admin/images/icons/gestioncat.png" alt="icon" /><br />
-					Manage the categorie
-				</span></a></li>
-				
-				<li><a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=add_cat"><span>
-					<img src="modules/Admin/images/icons/paper_content_pencil_48.png" alt="icon" /><br />
-					Add a categorie
-				</span></a></li>
-				
-				<li><a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=main_broken"><span>
-					<img src="modules/Admin/images/icons/corbeille.png" alt="icon" /><br />
-					Manage the brokens links
-				</span></a></li>
-				
-				<li><a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=main_pref"><span>
-					<img src="modules/Admin/images/icons/preference.png" alt="icon" /><br />
-					Preference
-				<br />&nbsp;</span></a></li>
+	<li>
+		<a class="shortcut-button" href="index.php?file=Links&amp;page=admin">
+			<img src="modules/Admin/images/icons/speedometer.png" alt="icon" />
+			<span>Manage the module</span>
+		</a>
+	</li>				
+	<li>
+		<a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=add_link">
+			<img src="modules/Admin/images/icons/add_link.png" alt="icon" />
+			<span>Add a link</span>
+		</a>
+	</li>				
+	<li>
+		<a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=main_cat">
+			<img src="modules/Admin/images/icons/folder_full.png" alt="icon" />
+			<span>Manage folders</span>
+		</a>
+	</li>				
+	<li>
+		<a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=add_cat">
+			<img src="modules/Admin/images/icons/add_to_folder.png" alt="icon" />
+			<span>Add a folder</span>
+		</a>
+	</li>				
+	<li>
+		<a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=main_broken">
+			<img src="modules/Admin/images/icons/remove_link.png" alt="icon" />
+			<span>Manage broken links</span>
+		</a>
+	</li>				
+	<li>
+		<a class="shortcut-button" href="index.php?file=Links&amp;page=admin&amp;op=main_pref">
+			<img src="modules/Admin/images/icons/process.png" alt="icon" />
+			<span>Preferences</span>
+		</a>
+	</li>
 </body></html>
