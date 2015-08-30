56539f338e2b7c060f8f7913faf6320c549e5fac
Determe Stive <stivedeterme@msn.com>
2015-08-30 21:29:21 +0200

    * replace HTML code in html5

--- modules/404/index.php
+++ modules/404/index.php
@@ -9,32 +9,34 @@
 //-------------------------------------------------------------------------//
 defined('INDEX_CHECK') or die ('You can\'t run this file alone.');
 
-global $nuked, $language;
-translate('modules/Search/lang/' . $language . '.lang.php');
-translate('modules/404/lang/' . $language . '.lang.php');
+translate('modules/Search/lang/<?php echo $GLOBALS['language'; ?>.lang.php');
+translate('modules/404/lang/<?php echo $GLOBALS['language'; ?>.lang.php');
 
-if($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin'){
-	include('modules/Admin/design.php');
-	admintop();
-}else{
-	opentable();
+if ($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin') {
+    include('modules/Admin/design.php');
+    admintop();
+} else{
+    opentable();
 }
 
-$error_title = ($_REQUEST['op'] != 'sql') ? '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />' : _ERROR404SQL . '<br /><br />';
-
-echo '<div style="text-align: center; padding: 0 10px">' . $error_title . '
-          <form method="post" action="index.php?file=Search&amp;op=mod_search">
-              <p><input type="hidden" name="module" value="" /><input type="text" name="main" size="25" /></p>
-              <p><input type="submit" class="button" name="submit" value="' . _SEARCHFOR . '" /></p>
-              <p><a href="index.php?file=Search"><b>' . _ADVANCEDSEARCH . '</b></a> - <a href="javascript:history.back()"><b>' . _BACK . '</b></a></p>
-		  </form>
-	  </div>';
-
-if($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin'){
-	include('modules/Admin/design.php');
-	adminfoot();
-}else{
-	closetable();
-}
-
+$error_title = ($_REQUEST['op'] != 'sql') ? '
+    <p><big><strong>'.$GLOBALS['nuked']['name'].'</strong></big></p>
+    <p><?php echo _NOEXIST; ?></p>' : '<p>'._ERROR404SQL . '</p>';
 ?>
+    <div style="text-align: center; padding: 0 10px"><?php echo $error_title; ?>
+        <form method="post" action="index.php?file=Search&amp;op=mod_search">
+            <p><input type="hidden" name="module" value=""><input type="text" name="main" size="25"></p>
+            <p><input type="submit" class="button" name="submit" value="<?php echo _SEARCHFOR; ?>"></p>
+            <p>
+                <a href="index.php?file=Search"><strong><?php echo _ADVANCEDSEARCH; ?></strong></a> -
+                <a href="javascript:history.back()"><strong><?php echo _BACK: ?></strong></a>
+            </p>
+        </form>
+    </div>
+<?php
+
+if ($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin') {
+    include('modules/Admin/design.php'); adminfoot();
+} else {
+    closetable();
+}
