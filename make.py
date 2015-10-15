e8f534bb60246e2c8592b397b1770cd33a888fad
cyrillePrigent <cyrille.prigent@laposte.net>
2015-10-15 02:24:09 +0200

    - Cleaning .gitignore file   Create your personal global .gitignore if needed (see https://help.github.com/articles/ignoring-files/)   More exemple : https://github.com/github/gitignore - Add more removed file for make.py

--- make.py
+++ make.py
@@ -20,26 +20,36 @@ def _getVersion():
 
 def _ignore(src, name):
     if ((src == './UPLOAD/') or (src == './/UPLOAD/')):
-        print (name)
-        return ['RESTDIR', 'conf.inc.php', file, 'Thumbs.db', '.DS_Store', '.gitignore', '.git', '.hg', '.svn', 'make.py']
+        return [['RESTDIR', 'conf.inc.php', file, 'Thumbs.db', 'ehthumbs.db', '.DS_Store', '.Spotlight-V100', '.Trashes', '.gitignore', '.git', 'make.py'], ['config_save_.*\.php', '*\.bak', 'thumbcache_.*\.db']]
     else:
-        return []
+        return [[], []]
 
 def _RecImport(src, dst, zip):
     elements = os.listdir(src)
     ignored = _ignore('.' + dst, elements)
-    for delete in ignored:
+
+    for delete in ignored[0]:
         try:
             elements.remove(delete)
         except ValueError:
             pass
 
     for ele in elements:
-        if os.path.isfile(src + ele):
-            zip.write(src + ele, dst + ele)
-            print (dst + ele)
-        elif os.path.isdir(src + ele):
-            _RecImport(src + ele + '/', dst + ele + '/', zip)
+        remove = False
+
+        for delete in ignored[1]:
+            test = re.search(delete, ele)
+
+            if test :
+                remove = True
+                break
+
+        if remove is not True:
+            if os.path.isfile(src + ele):
+                zip.write(src + ele, dst + ele)
+                print (dst + ele)
+            elif os.path.isdir(src + ele):
+                _RecImport(src + ele + '/', dst + ele + '/', zip)
 
 
 version = _getVersion()
