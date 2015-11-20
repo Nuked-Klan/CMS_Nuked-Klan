18b8c7d4875742c4dd72e95180ce6e125551ab8a
cyrillePrigent <cyrille.prigent@laposte.net>
2015-11-21 00:33:44 +0100

    Add view files of admin design
    Missing in last commit (Oooops) :
    - Integration of Admin design in nkTemplate
    - Fix PHP notice in Restless theme
    - Correction of nkAlert message displayed with nude design

---
+++ views/backend/design/footerAdmin.php
@@ -0,0 +1,93 @@
+                <script type="text/javascript" src="media/js/syntaxhighlighter/shCore.js"></script>
+                <script type="text/javascript" src="media/js/syntaxhighlighter/shAutoloader.js"></script>
+                <script type="text/javascript" src="media/js/syntaxhighlighter.autoloader.js"></script>
+                <link type="text/css" rel="stylesheet" href="media/css/syntaxhighlighter/shCore.css"/>
+                <link type="text/css" rel="stylesheet" href="media/css/syntaxhighlighter/shThemeDefault.css"/>
+
+<?php
+    // choix de l'éditeur
+    if ($GLOBALS['nuked']['editor_type'] == 'cke') : //ckeditor
+?>
+                    <script type="text/javascript" src="media/ckeditor/ckeditor.js"></script>
+                    <script type="text/javascript" src="media/ckeditor/config.js"></script>
+                    <script type="text/javascript">
+                    //<![CDATA[
+                        <?php echo ($GLOBALS['nuked']['video_editeur'] == 'on') ? 'CKEDITOR.config.extraPlugins = "Video";' : '' ?>
+                        CKEDITOR.config.scayt_sLang = "<?php echo ($GLOBALS['language'] == 'french') ? 'fr_FR' : 'en_US' ?>";
+                        <?php echo ($GLOBALS['nuked']['scayt_editeur'] == 'on') ? 'CKEDITOR.config.scayt_autoStartup = "true";' : '' ?>
+                        CKEDITOR.replaceAll(function(textarea,config){
+                            if (textarea.className!='editor') return false;
+                            CKEDITOR.config.toolbar = 'Full';
+                            CKEDITOR.config.autoGrow_onStartup = true;
+                            CKEDITOR.config.autoGrow_maxHeight = 200;
+                            CKEDITOR.configlanguage = '<?php echo substr($GLOBALS['language'], 0, 2) ?>';
+                            <?php echo (! empty($GLOBALS['bgcolor4'])) ? 'CKEDITOR.config.uiColor = "'. $GLOBALS['bgcolor4'] .'";' : ''; ?>
+                            CKEDITOR.config.allowedContent=
+                                'p h1 h2 h3 h4 h5 h6 blockquote tr td div a span{text-align,font-size,font-family,font-style,color,background-color,display};' +
+                                'img[!src,alt,width,height,class,id,style,title,border];' +
+                                'strong s em u strike sub sup ol ul li br caption thead  hr big small tt code del ins cite q address section aside header;' +
+                                'div[class,id,style,title,align]{page-break-after,width,height,background};' +
+                                'a[!href,accesskey,class,id,name,rel,style,tabindex,target,title];' +
+                                'table[align,border,cellpadding,cellspacing,class,id,style];' +
+                                'td[colspan, rowspan];' +
+                                'th[scope];' +
+                                'pre(*);' +
+                                'span[id, style];'
+<?php
+        if ($GLOBALS['nuked']['video_editeur'] == 'on') : ?>
+                                    + 'object[width,height,data,type];'
+                                    + 'param[name,value];'
+                                    + 'embed[width,height,src,type,allowfullscreen,allowscriptaccess];'
+<?php
+        endif
+?>
+                                ;
+                            });
+<?php
+        if ($_REQUEST['file'] == 'Forum' && ($_REQUEST['op'] == 'edit_forum' || $_REQUEST['op'] == 'add_forum')) :
+?>
+                            CKEDITOR.config.autoParagraph = false;
+                            CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
+<?php
+        endif;
+
+        echo ConfigSmileyCkeditor();
+?>
+                    //]]>
+                    </script>
+<?php
+    elseif ($GLOBALS['nuked']['editor_type'] == 'tiny') : //tinymce
+?>
+                    <script type="text/javascript" src="media/tinymce/tinymce.min.js"></script>
+                    <script type="text/javascript">
+                        //<![CDATA[
+                        tinymce.init({
+                            selector: "textarea.editor",
+                            language : 'fr_FR',
+                            plugins: [
+                                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                                "searchreplace wordcount visualblocks visualchars code fullscreen",
+                                "insertdatetime media nonbreaking save table contextmenu directionality",
+                                "emoticons paste textcolor responsivefilemanager youtube"
+                            ],
+                            image_advtab: true,
+                            toolbar1: "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | media youtube link image emoticons responsivefilemanager | preview",
+                            /* toolbar2: "print preview media | forecolor backcolor emoticons | link image", */
+
+                           external_filemanager_path: 'media/filemanager/',
+                           filemanager_title: 'Gestion des fichiers',
+                           external_plugins: { 'filemanager' : '../filemanager/plugin.min.js' }
+
+                         });
+                    //]]>
+                    </script>
+<?php
+    endif
+?>
+            </div>
+        </div>
+        <!-- End Main Content -->
+
+    </div>
+    <!-- End #body-wrapper -->
+    </div>
---
+++ views/backend/design/topAdmin.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+    <head>
+        <meta name="keywords" content="<?php echo $GLOBALS['nuked']['keyword'] ?>" />
+        <meta name="Description" content="<?php echo $GLOBALS['nuked']['description'] ?>" />
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <title><?php echo $GLOBALS['nuked']['name'] ?> - <?php echo $GLOBALS['nuked']['slogan'] ?></title>
+
+        <link rel="shortcut icon"  href="images/favicon.ico" />
+        <link rel="stylesheet" href="modules/Admin/css/reset.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="modules/Admin/css/style.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="modules/Admin/css/invalid.css" type="text/css" media="screen" />
+
+        <script type="text/javascript" src="modules/Admin/scripts/jquery-1.6.1.min.js"></script>
+        <script type="text/javascript" src="modules/Admin/scripts/simpla.jquery.configuration.js"></script>
+        <script type="text/javascript" src="modules/Admin/scripts/facebox.js"></script>
+        <script type="text/javascript">
+            var condition_js = '<?php echo ($GLOBALS['nuked']['screen']) == 'off' ? 1 : 0 ?>';
+            var lang_nuked = '<?php echo $GLOBALS['language'] ?>';
+            </script>
+        <script type="text/javascript" src="modules/Admin/scripts/config.js"></script>
+    </head>
+    <body>
+        <div id="screen" onclick="screenoff()">
+            <div id="iframe"></div>
+            <div id="iframe_close">&nbsp;</div>
+        </div>
+
+        <div id="Frame">
+        <!-- Wrapper for the radial gradient background -->
+        <div id="body-wrapper">
+
+            <div id="sidebar">
+
+                <!-- Sidebar with logo and menu -->
+                <div id="sidebar-wrapper">
+
+                    <!-- Logo NK -->
+                    <a href="http://www.nuked-klan.org" target="_blank"><img id="logo" src="modules/Admin/images/logo.png" alt="Simpla Admin logo" /></a>
+
+                    <!-- Sidebar Profile links -->
+                    <div id="profile-links">
+                        <a href="index.php?file=User" title="<?php echo _EDIT ?>"><img src="<?php echo getUserAvatar() ?>" class="nkAdminUserAvatar" /></a>
+                        <?php echo _BONJOUR ?>
+                        <a href="index.php?file=User" title="<?php echo _EDIT ?>"><?php echo $GLOBALS['user']['name'] ?></a><br />
+                        <?php echo _VOIR ?>
+                        <a href="#messages" rel="modal"><?php echo _MESSAGES ?></a><br /><br />
+<?php
+    if ($GLOBALS['nuked']['screen'] == 'on') :
+?>
+                            <a onclick="javascript:screenon('index.php', 'non');return false" href="#"><?php echo _VOIRSITE ?></a> |
+<?php
+    endif
+?>
+                        <a href="index.php?file=Admin&amp;page=deconnexion"><?php echo _DECONNEXION ?></a><br />
+                        <a href="index.php"><?php echo _RETOURNER ?></a>
+                    </div>
+
+                <!-- Accordion Menu -->
+                <ul id="main-nav">
+
+                    <li><a href="index.php?file=Admin" class="nav-top-item no-submenu<?php printAdminMenuCurrentClass('pannel') ?>"><?php echo _PANNEAU ?></a></li>
+
+                    <li>
+                        <!-- SUB MENU : PARAMETERS -->
+                        <a href="#" class="nav-top-item<?php printAdminMenuCurrentClass('parameter') ?>"><?php echo _PARAMETRE; ?></a>
+
+                        <ul>
+                            <li><a <?php printAdminSubMenuCurrentClass('setting') ?> href="index.php?file=Admin&amp;page=setting"><?php echo _PREFGEN ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('mysql') ?> href="index.php?file=Admin&amp;page=mysql"><?php echo _GMYSQL ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('phpinfo') ?> href="index.php?file=Admin&amp;page=phpinfo"><?php echo _ADMINPHPINFO ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('action') ?> href="index.php?file=Admin&amp;page=action"><?php echo _ACTIONM ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('erreursql') ?> href="index.php?file=Admin&amp;page=erreursql"><?php echo _ERRORBDD ?></a></li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <!-- SUB MENU : GESTION -->
+                        <a href="#" class="nav-top-item<?php printAdminMenuCurrentClass('management') ?>"><?php echo _GESTIONS ?></a>
+
+                        <ul>
+                            <li><a <?php printAdminSubMenuCurrentClass('user') ?> href="index.php?file=Admin&amp;page=user"><?php echo _UTILISATEURS ?></a></li>
+
+<?php
+    if (file_exists('themes/'. $GLOBALS['nuked']['theme'] .'/admin.php')) :
+?>
+                                <li><a <?php printAdminSubMenuCurrentClass('theme') ?> href="index.php?file=Admin&amp;page=theme"><?php echo _THEMIS ?></a></li>
+<?php
+    endif
+?>
+
+                            <li><a <?php printAdminSubMenuCurrentClass('modules') ?> href="index.php?file=Admin&amp;page=modules"><?php echo _INFOMODULES ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('block') ?> href="index.php?file=Admin&amp;page=block"><?php echo _BLOCK ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('menu') ?> href="index.php?file=Admin&amp;page=menu"><?php echo _MENU ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('smilies') ?> href="index.php?file=Admin&amp;page=smilies"><?php echo _SMILEY ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('games') ?> href="index.php?file=Admin&amp;page=games"><?php echo _JEUX ?></a></li>
+                        </ul>
+                    </li>
+                    <!-- SUB MENU : CONTENU -->
+                    <li>
+<?php
+    list($moduleCurrentClass, $modulesList) = getAdminModulesMenuData();
+?>
+                        <a href="#" class="nav-top-item<?php echo $moduleCurrentClass ?>"><?php echo _CONTENU ?></a>
+                        <ul>
+<?php
+    foreach ($modulesList as $module => $moduleName) :
+        if ($_REQUEST['file'] == $module && $_REQUEST['page'] == 'admin') :
+?>
+                            <li><a class="current" href="index.php?file=<?php echo $module ?>&amp;page=admin"><?php echo $moduleName ?></a></li>
+<?php
+        else :
+?>
+                            <li><a href="index.php?file=<?php echo $module ?>&amp;page=admin"><?php echo $moduleName ?></a></li>
+<?php
+        endif;
+    endforeach;
+?>
+
+                        </ul>
+                    </li>
+                    <li>
+                        <!-- SUB MENU : DIVERS -->
+                        <a href="#" class="nav-top-item<?php printAdminMenuCurrentClass('miscellaneous') ?>"><?php echo _DIVERS ?></a>
+                        <ul>
+                            <li><a href="http://www.nuked-klan.org/index.php?file=Forum" target="_blank"><?php echo _OFFICIEL ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('licence') ?> href="index.php?file=Admin&amp;page=licence"><?php echo _LICENCE ?></a></li>
+                            <li><a <?php printAdminSubMenuCurrentClass('propos') ?> href="index.php?file=Admin&amp;page=propos"><?php echo _PROPOS ?></a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- End Accordion Menu -->
+                <!-- Messages are shown when a link with these attributes are clicked: href="#messages" rel="modal"  -->
+                <div id="messages">
+                    <h3><?php echo _DISCUADMIN ?>:</h3>
+                    <div id="content_messages">
+
+<?php
+    foreach (getDiscussionData() as $discussion) :
+?>
+                            <p><strong><?php echo nkDate($discussion['date']) ?></strong> <?php echo _BY ?> <?php echo $discussion['pseudo'] ?><br /><?php echo $discussion['texte'] ?></p>
+<?php
+    endforeach
+?>
+
+                    </div>
+                    <form method="post" onsubmit="maFonctionAjax(this.texte.value);return false" action="">
+                        <h4><?php echo _NEWMSG ?>:</h4>
+                        <fieldset>
+                            <textarea name="texte" cols="79" rows="5"></textarea>
+                        </fieldset>
+                        <fieldset>
+                            <input class="button" type="submit" value="Send" />
+                        </fieldset>
+                    </form>
+                    <div id="affichefichier"></div>
+                </div>
+                <!-- End #messages -->
+
+            </div>
+            <!-- End #sidebar-wrapper -->
+
+        </div>
+        <!-- End #sidebar -->
+
+        <!-- Main Content Section with everything -->
+        <div id="main-content">
+            <div style="width:100%;height:100%;display:block">
+                <!-- Show a notification if the user has disabled javascript -->
+                <noscript>
+                    <div class="notification error png_bg">
+                        <div><?php echo _JAVA ?></div>
+                    </div>
+                </noscript>
