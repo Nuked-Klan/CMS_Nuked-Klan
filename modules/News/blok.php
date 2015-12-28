8da7ae3263b023999efaff92f2aee0ed67be7ca9
cyrillePrigent <cyrille.prigent@laposte.net>
2015-12-28 01:07:45 +0100

    Clean blok.php file of News module for author link

--- modules/News/blok.php
+++ modules/News/blok.php
@@ -21,14 +21,21 @@ $margin = ($active == 3 or $active == 4) ? '5px' : '11px';
 
 echo '<ul style="margin:5px ' . $margin . ';padding:5px;' . $listStyle . '">';
 
-$Sql = mysql_query("SELECT N.id, N.titre, N.date, N.auteur, N.auteur_id, U.pseudo FROM " . NEWS_TABLE . " N, " . USER_TABLE . " U WHERE '$day' >= N.date AND U.id = N.auteur_id ORDER BY date DESC LIMIT 0, 5");
+$Sql = mysql_query("SELECT id, titre, date, auteur, auteur_id FROM " . NEWS_TABLE . " WHERE '$day' >= date ORDER BY date DESC LIMIT 0, 5");
 while ($row = mysql_fetch_assoc($Sql)) {
 	
 	$row['date'] = nkDate($row['date']);
 	$row['titre'] = printSecuTags($row['titre']);
 	$titre = (strlen($row['titre']) > 30) ? substr($row['titre'],0,30).'...' : $row['titre'];
-	$auteur = (!empty($row['auteur_id'])) ? $row['pseudo'] : $row['auteur'];
-	$title = _BY . ' ' . $auteur . ' ( ' . $row['date'] . ' )';
+
+    if ($row['auteur_id'] != '') {
+        $authorLink = '<a href="index.php?file=Members&amp;op=detail&amp;autor='. urlencode($row['auteur']) .'">'. $row['auteur'] .'</a>';
+    }
+    else {
+        $authorLink = $row['auteur'];
+    }
+
+	$title = _BY . ' ' . $row['auteur'] . ' ( ' . $row['date'] . ' )';
 	
 	if ($active == 3 or $active == 4) {
 		
@@ -39,7 +46,7 @@ while ($row = mysql_fetch_assoc($Sql)) {
 				          <a href="index.php?file=News&amp;op=index_comment&amp;news_id=' . $row['id'] . '" title="' . $row['titre'] . '"><b>' . $row['titre'] . '</b></a>
 				      </h2>
 				      <p style="margin:0;padding:0">
-					      ' . _BY . ' <a href="index.php?file=Team&amp;op=detail&amp;autor=' . urlencode($auteur) . '" title="">' . $auteur . '</a> ( ' . $row['date'] . ' )
+					      ' . _BY . ' '. $authorLink .' ( ' . $row['date'] . ' )
 					  </p>
 				  </div>
 				  <hr style="color: ' . $bgcolor3 . ';height:1px;clear:both" />
