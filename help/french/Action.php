02b409fecfe55e3ac8e8747cd4b61ea2117871c8
cyrillePrigent <cyrille.prigent@laposte.net>
2015-12-30 03:21:20 +0100

    Start cleaning and formating Help files #95

--- help/french/Action.php
+++ help/french/Action.php
@@ -1,18 +1,22 @@
-<?php header('Content-type: text/html; charset=iso-8859-1'); ?><html>
-<head>
-<title>Aides Administration</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-</head>
-
-<body style="font-family: Verdana; font-size: 10px" link="#000000" vlink="#000000" alink="#000000" bgcolor="#FFFFFF">
-<center><big><b>Action</b></big></center><br>
-
-<li><b>Index</b> : Dans cette partie, vous avez la liste de toutes les modifications ou connexion ou déconnexion que les administrateurs ont effectué sur le site...
-Cette liste se vide automatiquement dès qu'une action dépasse les 2 semaines depuis sa création, afin d'empêcher un administrateur de vider les actions.</li><br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-</body></html>
+<?php header('Content-type: text/html; charset=iso-8859-1'); ?>
+<html>
+    <head>
+        <title>Aides Administration</title>
+        <meta http-equiv="content-style-type" content="text/css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <link title="css" rel="stylesheet" type="text/css" href="../style.css" media="screen" />
+    </head>
+    <body>
+        <div style="text-align:center;"><big><b>Action</b></big></div><br />
+        <ul>
+            <li><b>Index</b> : Dans cette partie, vous avez la liste de toutes les modifications ou connexion ou déconnexion que les administrateurs ont effectué sur le site...Cette liste se vide automatiquement dès qu'une action dépasse les 2 semaines depuis sa création, afin d'empêcher un administrateur de vider les actions.</li>
+        </ul>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    </body>
+</html>
