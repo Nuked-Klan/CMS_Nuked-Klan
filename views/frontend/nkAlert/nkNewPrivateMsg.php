70398fde46fc2b03cf10e2c256ecc35606a8a000
cyrillePrigent <cyrille.prigent@laposte.net>
2016-03-10 13:32:12 +0100

    Fix nkAlert for new private message

--- views/frontend/nkAlert/nkNewPrivateMsg.php
+++ views/frontend/nkAlert/nkNewPrivateMsg.php
@@ -1,5 +1,5 @@
                 <div id="nkNewPrivateMsg" class="nkAlert">
-                    <strong><?php echo _n('NEW_PRIVATE_MESSAGE', $GLOBALS['user']['nbNewPM']) ?></strong>
+                    <strong><?php printf(_n('NEW_PRIVATE_MESSAGE', $GLOBALS['user']['nbNewPM']), $GLOBALS['user']['nbNewPM']) ?></strong>
                     <a href="index.php?file=Userbox"><span><?php echo __('GO_TO_PRIVATE_MESSAGES') ?></span></a>
                     <a id="nkNewPrivateMsgClose" href="#" title="<?php echo __('CLOSE_WINDOW') ?>"><span><?php echo __('CLOSE_WINDOW') ?></span></a>
                 </div>
